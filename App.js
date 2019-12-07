6f9e73f130f5d5e9f0ac5ea7158665b7bca099e6
Bakare Sulaimon <ecomputer83@gmail.com>
2019-12-07 10:42:33 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { Block, GalioProvider } from 'galio-framework';
+
+import Screens from './navigation/Screens';
+import { Images, articles, nowTheme } from './constants';
+
+// cache app images
+const assetImages = [
+  Images.Onboarding,
+  Images.Logo,
+  Images.Pro,
+  Images.NowLogo,
+  Images.iOSLogo,
+  Images.androidLogo,
+  Images.ProfilePicture,
+  Images.CreativeTimLogo,
+  Images.InvisionLogo,
+  Images.RegisterBackground,
+  Images.ProfileBackground
+];
+
+// cache product images
+articles.map(article => assetImages.push(article.image));
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    }
+  });
+}
+
+export default class App extends React.Component {
+  state = {
+    fontLoaded: false
+  };
+
+  async componentDidMount() {
+    _loadResourcesAsync();
+  }
+
+  render() {
+  
+      return (
+        <GalioProvider theme={nowTheme}>
+          <Block flex>
+            <Screens />
+          </Block>
+        </GalioProvider>
+      );
+    
+  }
+
+  _loadResourcesAsync = async () => {
+    return Promise.all([...cacheImages(assetImages)]);
+  };
+}
